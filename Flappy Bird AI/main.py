b58c5dbad43add9e09c0d102e8b14de2427bca9c
BruceTsai0909 <Whitedragon0909@gmail.com>
2023-08-03 17:40:49 +0800

    add Pipe object

--- Flappy Bird AI/main.py
+++ Flappy Bird AI/main.py
@@ -84,6 +84,39 @@ class Bird:
 
     def get_mask(self):
         return pygame.mask.from_surface(self.img)
+    
+class Pipe:
+    GAP = 200
+    VEL = 5
+
+    def __init__(self, x):
+        self.x = x
+        self.height = 0
+
+        self.top = 0
+        self.bottom = 0
+        self.PIPE_TOP = pygame.transform.flip(PIPE_IMG, False, True)
+        self.PIPE_BOTTOM = PIPE_IMG 
+        
+        self.passed = False
+        self.set_height()
+    
+    def set_height(self):
+        self.height = random.randrange(50, 450)
+        self.top = self.height - self.PIPE_TOP.get_height() #lefttop corner of the top pipe
+        self.bottom = self.height + self.GAP #lefttop corner of the bottom pipe
+    
+    def move(self):
+        self.x -= self.VEL
+    
+    def draw(self, win):
+        win.blit(self.PIPE_TOP, (self.x, self.top))
+        win.blit(self.PIPE_BOTTOM, (self.x, self.bottom))
+
+    def collide(self): pass
+
+
+
 
 def draw_window(win, bird):
     win.blit(BG_IMG, (0, 0))
