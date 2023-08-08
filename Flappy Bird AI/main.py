40baf4f6b976551643025671f910807e7d83153e
BruceTsai0909 <Whitedragon0909@gmail.com>
2023-08-08 15:28:09 +0800

    add collide for pipe

--- Flappy Bird AI/main.py
+++ Flappy Bird AI/main.py
@@ -113,9 +113,20 @@ class Pipe:
         win.blit(self.PIPE_TOP, (self.x, self.top))
         win.blit(self.PIPE_BOTTOM, (self.x, self.bottom))
 
-    def collide(self): pass
+    def collide(self, bird=Bird):
+        bird_mask = bird.get_mask()
+        top_mask = pygame.mask.from_surface(self.PIPE_TOP)
+        bottom_mask = pygame.mask.from_surface(self.PIPE_BOTTOM)
 
+        top_offset = (self.x - bird.x, self.top - round(bird.y))
+        bottom_offset = (self.x - bird.x, self.bottom - round(bird.y))
 
+        b_point = bird_mask.overlap(bottom_mask, bottom_offset)
+        t_point = bird_mask.overlap(top_mask, top_offset)
+
+        if t_point or b_point: #once collide, return true
+            return True
+        return False
 
 
 def draw_window(win, bird):
